d4905c0d8c4876e979f21e2312575fff44b18d8b
Manoj Khati <mksea18@gmail.com>
2024-12-31 17:00:07 +0545

    setting updated

--- src/Setting.jsx
+++ src/Setting.jsx
@@ -5,8 +5,34 @@ const Setting=()=> {
       <h2 className='text-lg font-bold p-6 bg-white'>Settings</h2>
       <div  className='p-10 bg-white rounded shadow space-y-4'>
         <h3 className='font-bold text-lg'>Profile settings</h3>
+        <div className='grid grid-cols-2 gap-4'>
+          <div>
+            <label>Name</label>
+            <input
+            type='text'
+            value="Manoj Khati"
+            className='w-full p-2 border-gray-300 rounded'
+            disabled
+            />
+          </div>
+          <div>
+            <label>Email</label>
+            <input
+            type='email'
+            value="manojkhati321@gmail.com"
+            className='w-full p-2 border-gray-300 rounded'
+            disabled
+            />
+          </div>
+
+        </div>
 
       </div>
+       {/* Security Settings Section */}
+       <div>
+        <h3>Security Settings</h3>
+       </div>
+
       
     </div>
   )
